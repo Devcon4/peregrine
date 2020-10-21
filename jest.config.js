e7a7cf2b6c339b977c24d530a7fafba461f5d830
Devyn Cyphers <devyncyphers@gmail.com>
2020-10-20 22:52:34 -0600

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@jest/types/build/Config').DefaultOptions} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  
+};
+
+module.exports = config;
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+
+export function app() {
+    var res: string = `4`;
+    console.log(res);
+    return res;
+}
+
+app();
